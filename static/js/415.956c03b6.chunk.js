d3e414a6ebe65658ddc5cc4ff959ee1fa064e3bc
yanachepyk <119411033+yanachepyk@users.noreply.github.com>
2023-04-11 23:37:39 +0000

    Deploying to gh-pages from @ yanachepyk/goit-react-hw-08-phonebook@cf73dd191dba499d090f0dc763b8e017717e783a 🚀

---
+++ static/js/415.956c03b6.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[415],{415:function(e,n,o){o.r(n);var h=o(184);n.default=function(){return(0,h.jsx)("div",{children:(0,h.jsx)("h2",{children:"Home..."})})}}}]);
+//# sourceMappingURL=415.956c03b6.chunk.js.map
